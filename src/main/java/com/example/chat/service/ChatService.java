670f9bed0eddbee7d4c79bab6c18cf686f50a5ed
DevFrog92 <yongsu1481@gmail.com>
2023-08-10 17:45:06 +0900

    feat: implement ChatService(C)

---
+++ src/main/java/com/example/chat/service/ChatService.java
@@ -0,0 +1,53 @@
+package com.example.chat.service;
+
+import com.example.chat.domain.ChatRoom;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+
+    private final ObjectMapper objectMapper;
+    private Map<String, ChatRoom> chatRooms;
+
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    private List<ChatRoom> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+
+    private ChatRoom findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        String roomId = UUID.randomUUID().toString();
+        ChatRoom newRoom = ChatRoom.builder()
+                .roomId(roomId)
+                .name(name)
+                .build();
+
+        chatRooms.put(roomId, newRoom);
+        return newRoom;
+    }
+
+    public <T> void sendMessage(WebSocketSession session,
+                                T message) {
+        try {
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsBytes(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
