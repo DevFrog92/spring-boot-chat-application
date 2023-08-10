389a0c9f3c752d723d1eb92c475ef24232fc8e1c
DevFrog92 <yongsu1481@gmail.com>
2023-08-10 23:44:51 +0900

    feat: implement findAllRoom and createRoom api

---
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.example.chat.controller;
+
+import com.example.chat.domain.ChatRoom;
+import com.example.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatController {
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
