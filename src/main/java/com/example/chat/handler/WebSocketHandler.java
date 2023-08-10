59384b04329748e7c510db21911eb2751da24a0d
DevFrog92 <yongsu1481@gmail.com>
2023-08-10 23:40:58 +0900

    feat: update WebSocketHandler for payload

--- src/main/java/com/example/chat/handler/WebSocketHandler.java
+++ src/main/java/com/example/chat/handler/WebSocketHandler.java
@@ -1,5 +1,10 @@
 package com.example.chat.handler;
 
+import com.example.chat.domain.ChatRoom;
+import com.example.chat.dto.ChatMessageDto;
+import com.example.chat.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -8,18 +13,22 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     @Override
     protected void handleTextMessage(
             WebSocketSession session,
             TextMessage message) throws Exception {
-
         String payload = message.getPayload();
 
-        log.info(payload);
+        log.info("payload: {}", payload);
 
-        TextMessage testTextMessage = new TextMessage("Test text message");
-        session.sendMessage(testTextMessage);
+        ChatMessageDto chatMessageDto = objectMapper.readValue(payload, ChatMessageDto.class);
+        ChatRoom room = chatService.findRoomById(chatMessageDto.getRoomId());
+
+        room.handleActions(session, chatMessageDto, chatService);
     }
 }
