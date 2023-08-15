d892cf2f2a12d4a13d85faf4deb8fa4065a77e74
DevFrog92 <yongsu1481@gmail.com>
2023-08-15 17:07:17 +0900

    feat: update ChatController for RedisPublisher

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -2,24 +2,28 @@ package com.example.chat.controller;
 
 import com.example.chat.dto.ChatMessageDto;
 import com.example.chat.dto.ChatMessageType;
+import com.example.chat.pubsub.RedisPublisher;
+import com.example.chat.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessageDto messageDto) {
         if (ChatMessageType.JOIN.equals(messageDto.getType())) {
+            chatRoomRepository.enterChatRoom(messageDto.getRoomId());
             messageDto.setMessage(messageDto.getSender() + "님이 입장하셨습니다.");
         }
 
-        messagingTemplate.convertAndSend(
-                "/sub/chat/room/" + messageDto.getRoomId(),
-                messageDto);
+        redisPublisher.publish(
+                chatRoomRepository.getTopic(messageDto.getRoomId()),
+                messageDto
+        );
     }
 }
