68e4b028ea33b4374f0be51d8aff0c3c473afe0d
DevFrog92 <yongsu1481@gmail.com>
2023-08-16 23:52:53 +0900

    feat: add token parameter in message api

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -4,26 +4,36 @@ import com.example.chat.dto.ChatMessageDto;
 import com.example.chat.dto.ChatMessageType;
 import com.example.chat.pubsub.RedisPublisher;
 import com.example.chat.repository.ChatRoomRepository;
+import com.example.chat.service.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
-    private final RedisPublisher redisPublisher;
-    private final ChatRoomRepository chatRoomRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final JwtProvider jwtProvider;
+    private final ChannelTopic channelTopic;
 
     @MessageMapping("/chat/message")
-    public void message(ChatMessageDto messageDto) {
+    public void message(ChatMessageDto messageDto,
+                        @Header("token") String token) {
+
+        String username = jwtProvider.getUserNameFromJwt(token);
+        // 로그인 회원 정보로 대화면을 설정 -> 닉네임으로 바꿀 수 있도록 할 것인가?
+
+        messageDto.setSender(username);
+
         if (ChatMessageType.JOIN.equals(messageDto.getType())) {
-            chatRoomRepository.enterChatRoom(messageDto.getRoomId());
-            messageDto.setMessage(messageDto.getSender() + "님이 입장하셨습니다.");
+            messageDto.setSender("[알림]");
+            messageDto.setMessage(username + "님이 입장하셨습니다.");
         }
 
-        redisPublisher.publish(
-                chatRoomRepository.getTopic(messageDto.getRoomId()),
-                messageDto
-        );
+        // Websocket 에 발행된 메시지를 redis 로 발행 (publish)
+        redisTemplate.convertAndSend(channelTopic.getTopic(), messageDto);
     }
 }
