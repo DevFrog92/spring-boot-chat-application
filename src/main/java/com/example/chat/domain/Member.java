defb68b743ac11f89d09a426e2065d767560827a
DevFrog92 <yongsu1481@gmail.com>
2023-08-21 15:33:33 +0900

    feat: add Member entity

---
+++ src/main/java/com/example/chat/domain/Member.java
@@ -0,0 +1,27 @@
+package com.example.chat.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String nickname;
+
+    @Builder
+    public Member(String name, String nickname) {
+        this.name = name;
+        this.nickname = nickname;
+    }
+}
