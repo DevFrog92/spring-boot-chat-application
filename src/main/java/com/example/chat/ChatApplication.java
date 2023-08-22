beea0000093899018e94854148e8898e6745f208
DevFrog92 <yongsu1481@gmail.com>
2023-08-22 21:11:37 +0900

    feat: add dummy member bean and EnableJpaAuditing annotation

--- src/main/java/com/example/chat/ChatApplication.java
+++ src/main/java/com/example/chat/ChatApplication.java
@@ -1,8 +1,14 @@
 package com.example.chat;
 
+import com.example.chat.config.dummy.MemberDummyDataInit;
+import com.example.chat.repository.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChatApplication {
 
@@ -10,4 +16,9 @@ public class ChatApplication {
         SpringApplication.run(ChatApplication.class, args);
     }
 
+    @Bean
+    @Profile("test")
+    public MemberDummyDataInit memberTestDataInit(MemberRepository userRepository) {
+        return new MemberDummyDataInit(userRepository);
+    }
 }
