1fb87995df17991c81751896d7a293ea95d40114
DevFrog92 <yongsu1481@gmail.com>
2023-08-22 21:13:43 +0900

    refactor: rename LoginInfo to MemberInfo

--- src/main/java/com/example/chat/domain/MemberInfo.java
+++ src/main/java/com/example/chat/domain/MemberInfo.java
@@ -5,13 +5,13 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-public class LoginInfo {
+public class MemberInfo {
     private Long id;
     private String name;
     private String token;
 
     @Builder
-    public LoginInfo(Long id, String name, String token) {
+    public MemberInfo(Long id, String name, String token) {
         this.id = id;
         this.name = name;
         this.token = token;
