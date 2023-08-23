1be8238bcab92870ef1cea470007803f6383f2f9
DevFrog92 <yongsu1481@gmail.com>
2023-08-24 00:35:24 +0900

    feat: add getMemberCredentialOfRoom, submitSecretKey in ChatRoomController

--- src/main/java/com/example/chat/controller/ChatRoomController.java
+++ src/main/java/com/example/chat/controller/ChatRoomController.java
@@ -1,63 +1,95 @@
 package com.example.chat.controller;
 
-import com.example.chat.domain.ChatRoom;
-import com.example.chat.domain.LoginInfo;
-import com.example.chat.repository.ChatRoomRepository;
-import com.example.chat.service.JwtProvider;
+import com.example.chat.domain.Member;
+import com.example.chat.domain.Room;
+import com.example.chat.dto.ChatDeleteDto;
+import com.example.chat.dto.ChatKeyDto;
+import com.example.chat.dto.ResponseDto;
+import com.example.chat.service.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatRoomController {
-    private final ChatRoomRepository chatRoomRepository;
-    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
+    private final RoomService roomService;
+    private final ParticipationRoomService participationRoomService;
+
 
     @GetMapping("/room")
-    public String room(Model model) {
+    public String room() {
         return "/chat/room";
     }
 
     @GetMapping("/rooms")
     @ResponseBody
-    public List<ChatRoom> rooms() {
-        return chatRoomRepository.findAllRoom();
+    public List<Room> rooms(Authentication auth) {
+        return roomService.findAllRooms(auth.getName());
     }
 
     @PostMapping("/room")
     @ResponseBody
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatRoomRepository.createChatRoom(name);
+    public Room createRoom(@RequestParam String roomName,
+                           @RequestParam Long memberId) {
+        Member member = memberService.findMemberById(memberId);
+        return roomService.createRoom(member, roomName);
+    }
+
+    @PostMapping("/key")
+    public ResponseEntity<?> submitSecretKey(@RequestBody ChatKeyDto dto) {
+        // 이미 인증된 유저인지 확인한다.
+
+        if (roomService.checkSubmitSecretKey(dto)) {
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+
+        return new ResponseEntity<>(
+                HttpStatus.BAD_REQUEST
+        );
     }
 
     @GetMapping("/room/enter/{roomId}")
     public String roomDetail(Model model,
-                            @PathVariable String roomId) {
+                             @PathVariable String roomId) {
         model.addAttribute("roomId", roomId);
         return "/chat/roomDetail";
     }
 
     @GetMapping("/room/{roomId}")
     @ResponseBody
-    public ChatRoom roomInfo(@PathVariable String roomId) {
-        return chatRoomRepository.findRoomById(roomId);
+    public Room roomInfo(@PathVariable Long roomId) {
+        return roomService.getRoomInfo(roomId);
     }
 
-    @GetMapping("/user")
-    @ResponseBody
-    public LoginInfo getUserInfo() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String name = auth.getName();
-        return LoginInfo.builder()
-                .name(name)
-                .token(jwtProvider.generateToken(name))
-                .build();
+    @DeleteMapping("/room")
+    public void deleteRoom(@RequestBody ChatDeleteDto dto) {
+        roomService.deleteRoom(dto);
     }
+
+    @PostMapping("/room/credential")
+    public ResponseEntity<?> getMemberCredentialOfRoom(@RequestBody ChatKeyDto dto) {
+        Member member = memberService.findMemberById(dto.getMemberId());
+        Room room = roomService.findById(dto.getRoomId());
+        boolean permitMember = participationRoomService.isPermitMember(member, room);
+        return new ResponseEntity<>(
+                new ResponseDto<>(
+                        HttpStatus.OK.value(),
+                        "조회 완료",
+                        permitMember
+                ),
+                HttpStatus.OK
+        );
+    }
+
 }
