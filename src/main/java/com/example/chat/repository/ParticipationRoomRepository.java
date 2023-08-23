2ea21c44a1de19b224d93236d480cd5aeedb301d
DevFrog92 <yongsu1481@gmail.com>
2023-08-24 00:31:24 +0900

    feat: add deleteByRoomAndMember and findByMemberAndRoom in ParticipationRoomRepository

--- src/main/java/com/example/chat/repository/ParticipationRoomRepository.java
+++ src/main/java/com/example/chat/repository/ParticipationRoomRepository.java
@@ -1,10 +1,15 @@
 package com.example.chat.repository;
 
+import com.example.chat.domain.Member;
 import com.example.chat.domain.ParticipationRoom;
+import com.example.chat.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ParticipationRoomRepository extends JpaRepository<ParticipationRoom, Long> {
-    List<ParticipationRoom> findByMemberId(Long memberId);
+    Optional<ParticipationRoom> findByMemberAndRoom(Member member, Room room);
+    void deleteByRoomAndMember(Room room, Member member);
 }
