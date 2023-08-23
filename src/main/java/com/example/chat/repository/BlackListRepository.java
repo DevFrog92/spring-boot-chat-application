02d499902ca167342df6841be6d1d8a97a534196
DevFrog92 <yongsu1481@gmail.com>
2023-08-24 00:30:49 +0900

    feat: add findByRoomAndMember in BlackListRepository

--- src/main/java/com/example/chat/repository/BlackListRepository.java
+++ src/main/java/com/example/chat/repository/BlackListRepository.java
@@ -1,7 +1,12 @@
 package com.example.chat.repository;
 
 import com.example.chat.domain.BlackList;
+import com.example.chat.domain.Member;
+import com.example.chat.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BlackListRepository extends JpaRepository<BlackList, Long> {
+    Optional<BlackList> findByRoomAndMember(Room room, Member member);
 }
