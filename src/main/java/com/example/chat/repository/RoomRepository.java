e0ed78f64029d06219c6bfbcf0c1aa1a74314efd
DevFrog92 <yongsu1481@gmail.com>
2023-08-24 00:31:37 +0900

    feat: add findRoomsNotInBlacklistForUser in RoomRepository

--- src/main/java/com/example/chat/repository/RoomRepository.java
+++ src/main/java/com/example/chat/repository/RoomRepository.java
@@ -2,6 +2,13 @@ package com.example.chat.repository;
 
 import com.example.chat.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Query("SELECT c FROM Room c " +
+            "WHERE c.id NOT IN (SELECT b.room.id FROM BlackList b WHERE b.member.id = :memberId)")
+    List<Room> findRoomsNotInBlacklistForUser(@Param("memberId") Long memberId);
 }
