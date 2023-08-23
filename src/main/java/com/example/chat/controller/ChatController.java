6684c03b22ff1a28f62fe7a2cb0b8d42ec99353d
DevFrog92 <yongsu1481@gmail.com>
2023-08-24 00:35:03 +0900

    feat: add enter, leave, ban messageMapping in ChatController

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -1,42 +1,47 @@
 package com.example.chat.controller;
 
+import com.example.chat.dto.ChatBanDto;
+import com.example.chat.dto.ChatEnterDto;
 import com.example.chat.dto.ChatMessageDto;
-import com.example.chat.dto.ChatMessageType;
+import com.example.chat.service.ChatService;
 import com.example.chat.service.JwtProvider;
+import com.example.chat.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
-    private final RedisTemplate<String, Object> redisTemplate;
     private final JwtProvider jwtProvider;
-    private final ChannelTopic channelTopic;
+    private final ChatService chatService;
+    private final RoomService roomService;
 
     @MessageMapping("/chat/message")
-    public void message(ChatMessageDto messageDto,
+    public void message(@RequestBody ChatMessageDto messageDto,
                         @Header("token") String token) {
 
         String username = jwtProvider.getUserNameFromJwt(token);
-
-        // todo: change username to nickname
         messageDto.setSender(username);
+        chatService.sendChatMessage(messageDto);
+    }
 
-        if (ChatMessageType.JOIN.equals(messageDto.getType())) {
-            messageDto.setSender("[Notice 📣]");
-            messageDto.setMessage(username + " joined this chatroom.");
-        }
+    @MessageMapping("/chat/enter")
+    public void enter(@RequestBody ChatEnterDto enterDto) {
+        roomService.checkEntryExamination(enterDto);
+    }
 
-        // publishing to redis pub/sub
-        redisTemplate.convertAndSend(
-                channelTopic.getTopic(),
-                messageDto
-        );
+    @MessageMapping("/chat/leave")
+    public void leave(@RequestBody ChatEnterDto enterDto) {
+        roomService.leaveRoom(enterDto);
+    }
+
+    @MessageMapping("/chat/ban")
+    public void ban(@RequestBody ChatBanDto banDto) {
+        roomService.banMemberFromChatRoom(banDto);
     }
 }
