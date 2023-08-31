a1dbc407374a27f5dbad4107e5a114d4d73b1696
DevFrog92 <yongsu1481@gmail.com>
2023-08-31 20:34:00 +0900

    refactor: remove legacy files

--- src/main/java/com/example/chat/global/jwt/provider/JwtProvider.java
+++
@@ -1,7 +0,0 @@
-package com.example.chat.global.jwt.provider;
-
-public interface JwtProvider {
-    String generateToken(String key);
-    String getKeyFromToken(String token);
-    void validateToken(String token);
-}
