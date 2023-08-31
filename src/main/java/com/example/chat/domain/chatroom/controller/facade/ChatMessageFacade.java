f57429d7426e3d0c52d6ab9263b33a3b0448ac85
DevFrog92 <yongsu1481@gmail.com>
2023-08-31 16:54:17 +0900

    refactor: remove ChatMessageFacade and ChatMessageFacadeImpl

--- src/main/java/com/example/chat/domain/chatroom/controller/facade/ChatMessageFacade.java
+++
@@ -1,7 +0,0 @@
-package com.example.chat.domain.chatroom.controller.facade;
-
-public interface ChatMessageFacade {
-    void join(Long memberId, Long roomId);
-    void leave(Long memberId, Long roomId);
-    void ban(String memberName, Long roomId);
-}
