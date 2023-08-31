6b2a9bac43e499b0d1d419e63adb203e17edbb0b
DevFrog92 <yongsu1481@gmail.com>
2023-08-31 20:41:52 +0900

    feat: implement TokenHolder and TokenHolderImpl

---
+++ src/main/java/com/example/chat/domain/common/service/port/TokenHolder.java
@@ -0,0 +1,7 @@
+package com.example.chat.domain.common.service.port;
+
+public interface TokenHolder {
+    String generateToken(String key);
+    String getKeyFromToken(String token);
+    void validateToken(String token);
+}
