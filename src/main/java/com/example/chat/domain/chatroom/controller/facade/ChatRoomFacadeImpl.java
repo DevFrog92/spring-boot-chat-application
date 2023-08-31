61222e6c95456f1cac73e3b6ab1b28fb2dcadbe2
DevFrog92 <yongsu1481@gmail.com>
2023-08-31 16:55:19 +0900

    refactor: implement enter, ban and leave method

--- src/main/java/com/example/chat/domain/chatroom/controller/facade/ChatRoomFacadeImpl.java
+++ src/main/java/com/example/chat/domain/chatroom/controller/facade/ChatRoomFacadeImpl.java
@@ -2,25 +2,33 @@ package com.example.chat.domain.chatroom.controller.facade;
 
 import com.example.chat.domain.chatroom.domain.ChatRoom;
 import com.example.chat.domain.chatroom.domain.ChatRoomCreate;
-import com.example.chat.domain.chatroom.domain.ChatRoomType;
+import com.example.chat.domain.chatroom.domain.ParticipationRoom;
+import com.example.chat.domain.chatroom.dto.message.ChatMessageType;
 import com.example.chat.domain.chatroom.service.port.BlackListService;
 import com.example.chat.domain.chatroom.service.port.ChatRoomService;
+import com.example.chat.domain.chatroom.service.port.ChatService;
 import com.example.chat.domain.chatroom.service.port.ParticipationRoomService;
 import com.example.chat.domain.member.domain.Member;
 import com.example.chat.domain.member.service.port.MemberService;
 import com.example.chat.global.web.Exception.CustomIncorrectPassword;
+import com.example.chat.global.web.Exception.CustomRuntimeException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 import static com.example.chat.domain.chatroom.controller.facade.PermissionType.*;
+import static com.example.chat.domain.chatroom.domain.ChatRoomType.PRIVATE;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChatRoomFacadeImpl implements ChatRoomFacade {
     private final MemberService memberService;
+    private final ChatService chatService;
     private final ChatRoomService chatRoomService;
     private final BlackListService blackListService;
     private final ParticipationRoomService participationRoomService;
@@ -34,12 +42,84 @@ public class ChatRoomFacadeImpl implements ChatRoomFacade {
 
     @Override
     @Transactional
+    public void enter(Long memberId, Long roomId) {
+        Member member = memberService.getById(memberId);
+        ChatRoom chatRoom = chatRoomService.getById(roomId);
+
+        if (blackListService.isMemberInBlackList(member, chatRoom)) {
+            throw new CustomRuntimeException("정상적인 접근이 아닙니다.");
+        }
+
+        if (chatRoom.getType().equals(PRIVATE)) {
+            enterPrivate(member, chatRoom);
+        } else {
+            enterPublic(member, chatRoom);
+        }
+    }
+
+    @Transactional
+    public void enterPublic(Member member, ChatRoom chatRoom) {
+        if (participationRoomService.alreadyJoined(member, chatRoom)) {
+            return;
+        }
+
+        participationRoomService.create(member, chatRoom);
+        participationRoomService.join(member, chatRoom);
+        chatRoomService.updateParticipationNum(chatRoom.join());
+        chatService.sendEnterChatRoomMessage(chatRoom.getId(), member.getName());
+        chatService.sendChatRoomInfo(chatRoom.getId(), chatRoom.join().getParticipationNum());
+    }
+
+    @Transactional
+    public void enterPrivate(Member member, ChatRoom chatRoom) {
+        ParticipationRoom participationRoom = participationRoomService
+                .getByMemberAndRoom(member, chatRoom);
+
+        if (participationRoom == null) {
+            throw new CustomRuntimeException("정상적인 접근이 아닙니다.");
+        }
+
+        if (participationRoom.getJoinState()) {
+            return;
+        }
+
+        participationRoomService.join(member, chatRoom);
+        chatRoomService.updateParticipationNum(chatRoom.join());
+        chatService.sendEnterChatRoomMessage(chatRoom.getId(), member.getName());
+        chatService.sendChatRoomInfo(chatRoom.getId(), chatRoom.join().getParticipationNum());
+    }
+
+    @Override
+    @Transactional
+    public void leave(Long memberId, Long roomId) {
+        Member member = memberService.getById(memberId);
+        ChatRoom chatRoom = chatRoomService.getById(roomId);
+
+        chatRoomService.updateParticipationNum(chatRoom.leave());
+        participationRoomService.deleteByMemberAndRoom(member, chatRoom);
+        chatService.sendLeaveChatRoomMessage(chatRoom.getId(), member.getName());
+        chatService.sendChatRoomInfo(chatRoom.getId(), chatRoom.leave().getParticipationNum());
+    }
+
+    @Override
+    @Transactional
+    public void ban(String memberName, Long roomId) {
+        Member member = memberService.getByName(memberName);
+        ChatRoom chatRoom = chatRoomService.getById(roomId);
+
+        blackListService.create(chatRoom, member);
+        chatService.sendMessageToMemberTopic(member.getId(), ChatMessageType.BAN);
+    }
+
+    @Override
+    @Transactional
     public void delete(Long memberId, Long roomId) {
         Member member = memberService.getById(memberId);
         ChatRoom chatRoom = chatRoomService.getById(roomId);
 
         participationRoomService.deleteAllByRoom(chatRoom);
         chatRoomService.deleteRoom(member, chatRoom);
+        chatService.sendChatRoomDeleted(chatRoom.getId());
     }
 
     @Override
@@ -76,7 +156,8 @@ public class ChatRoomFacadeImpl implements ChatRoomFacade {
             return NOT_ALLOW;
         }
 
-        if (!participationRoomService.isCertifiedMember(requestMember, chatRoom)) {
+        if (Objects.equals(chatRoom.getType(), PRIVATE) &&
+                !participationRoomService.isCertifiedMember(requestMember, chatRoom)) {
             return NEED_PASSWORD;
         }
 
