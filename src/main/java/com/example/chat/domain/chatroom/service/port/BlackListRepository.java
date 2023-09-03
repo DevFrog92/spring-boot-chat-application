839c26069e964f52c6d9157e6172e3ba2686d2da
DevFrog92 <yongsu1481@gmail.com>
2023-09-03 10:46:08 +0900

    refactor: update parameter name

--- src/main/java/com/example/chat/domain/chatroom/service/port/BlackListRepository.java
+++ src/main/java/com/example/chat/domain/chatroom/service/port/BlackListRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface BlackListRepository {
     BlackList save(BlackList blackList);
-    Optional<BlackList> findByRoomAndMember(ChatRoom room, Member memberEntity);
+    Optional<BlackList> findByRoomAndMember(ChatRoom room, Member member);
 }
--- src/main/java/com/example/chat/domain/chatroom/service/port/ChatRoomService.java
+++ src/main/java/com/example/chat/domain/chatroom/service/port/ChatRoomService.java
@@ -1,7 +1,7 @@
 package com.example.chat.domain.chatroom.service.port;
 
 import com.example.chat.domain.chatroom.domain.ChatRoom;
-import com.example.chat.domain.chatroom.domain.ChatRoomCreate;
+import com.example.chat.domain.chatroom.dto.ChatRoomCreateDto;
 import com.example.chat.domain.member.domain.Member;
 
 import java.util.List;
@@ -9,12 +9,10 @@ import java.util.List;
 public interface ChatRoomService {
     ChatRoom getById(Long id);
 
-    ChatRoom create(Member member, ChatRoomCreate chatRoomCreate);
+    ChatRoom create(Member member, ChatRoomCreateDto chatRoomCreateDto);
 
     void updateParticipationNum(ChatRoom chatRoom);
 
     List<ChatRoom> findAllRooms(Member member);
     void deleteRoom(Member member, ChatRoom chatRoom);
-
-    ChatRoom getInfo(Long roomId);
 }
