e590922623de6bbb7a84f88e178a834e1156e8bb
DevFrog92 <yongsu1481@gmail.com>
2023-09-05 16:24:36 +0900

    refactor: update exceptions

--- src/main/java/com/example/chat/domain/chatroom/service/ChatRoomServiceImpl.java
+++ src/main/java/com/example/chat/domain/chatroom/service/ChatRoomServiceImpl.java
@@ -4,9 +4,9 @@ import com.example.chat.domain.chatroom.domain.ChatRoom;
 import com.example.chat.domain.chatroom.dto.ChatRoomCreateDto;
 import com.example.chat.domain.chatroom.service.port.ChatRoomRepository;
 import com.example.chat.domain.chatroom.service.port.ChatRoomService;
-import com.example.chat.domain.common.Exception.CustomRuntimeException;
-import com.example.chat.domain.member.domain.Member;
 import com.example.chat.domain.common.Exception.CustomNoSuchElementException;
+import com.example.chat.domain.common.Exception.CustomOutOfPermissionException;
+import com.example.chat.domain.member.domain.Member;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -45,7 +45,7 @@ public class ChatRoomServiceImpl implements ChatRoomService {
     @Transactional
     public void deleteRoom(Member member, ChatRoom chatRoom) {
         if (!Objects.equals(chatRoom.getOwnerId(), member.getId())) {
-            throw new CustomRuntimeException("채팅방 관리자만 채팅방을 삭제할 수 있습니다.");
+            throw new CustomOutOfPermissionException();
         }
 
         chatRoomRepository.deletById(chatRoom.getId());
@@ -54,6 +54,6 @@ public class ChatRoomServiceImpl implements ChatRoomService {
     @Override
     public ChatRoom getById(Long id) {
         return chatRoomRepository.getById(id)
-                .orElseThrow(() -> new CustomNoSuchElementException("요청한 ID의 채팅방을 찾을 수 없습니다"));
+                .orElseThrow(CustomNoSuchElementException::new);
     }
 }
